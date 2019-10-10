6b04755734bbaf8eee3600dfee9057b7557907b2
Patryk13 <koler.patryk13@gmail.com>
2019-10-10 17:52:08 +0200

    edit

--- js.js
+++ js.js
@@ -1,5 +1,12 @@
 $(function(){
   alert("Kurs JQ");
-  $('p').css("color", "red");
-  $('h3').css("color", "lightgreen");
+  $('#nav li a').css("color", 'red');
+  $("#nav a:eq(2)").css("color",'yellow'); //(tablica)filtry działają jak tablica.
+  $("a:gt(3)").css("color",'gray');// wszystkie elementy powyzej 4 elementu zostana sformatowane
+  $("a:even").css("color","green");// wszyskie odnosniki nie parzyste zostana sformatowane
+  $("#nav a:odd").css("color","black");// wszyskie odnosniki nie parzyste zostana sformatowane
+  $('input[value]').css("color",'blue');
+  $('p:contains(kurs)').css("color",'red');// wyszukiwanie contains(czego szukasz)
+  $('p:has(a)').css('color',"red"); //sformatywanie elementu wewnatrz selektora
+  $('p a:nth-child(1)').css("color",'red'); //sformatywanie pierwszego elementu
 });
